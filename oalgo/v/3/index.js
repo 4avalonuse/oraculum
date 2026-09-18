eebdcf0742a79cf584ea30d88458142dec593242
Ivan <4avalonuse@gmail.com>
2026-09-17 21:59:05 -0400

    chore: clean legacy code and archives
    Remove obsolete OAlgo versions, OChart archives/PHP API/cache, dead modules and legacy entrypoints.

--- oalgo/v/3/index.js
+++
@@ -1,6 +0,0 @@
-// oalgo/index.js
-import LegoUI from "./ui/lego.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  new LegoUI("app");
-});
--- oalgo/v/6/core/BridgeOChart.js
+++
@@ -1,7 +0,0 @@
-// core/BridgeOChart.js
-var BridgeOChart = {
-  updateChart: function(chartData) {
-    console.log("BridgeOChart: atualizar gráfico com", chartData);
-    // Aqui você poderia integrar Chart.js, se desejar
-  }
-};
